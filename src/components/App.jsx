a592e16c35669258398e8e844e46e626d108c4ff
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-08-15 22:17:09 +0300

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,19 +6,47 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactsList/ContactsList';
 
-import { TitleStyled, ContactsTitle, ConteinerStyled } from './App.styled';
+import {
+  TitleStyled,
+  ContactsTitle,
+  ConteinerStyled,
+  ButtonStyled,
+} from './App.styled';
 import { GlobalStyle } from './GlobalStyle';
+
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: initialContacts,
     filter: '',
   };
+  componentDidMount() {
+    const contactsFilter = localStorage.getItem('contacts');
+    if (contactsFilter !== null) {
+      this.setState({ contacts: JSON.parse(contactsFilter) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts: prevFilters } = prevState;
+    const { contacts: nextFilters } = this.state;
+
+    if (prevFilters !== nextFilters) {
+      localStorage.setItem('contacts', JSON.stringify(nextFilters));
+    }
+  }
+  restoreDeleted = () => {
+    this.setState({
+      contacts: initialContacts,
+    });
+  };
+
   onFormSubmit = ({ name, number }) => {
     const existingContact = this.state.contacts.find(
       contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim()
@@ -67,10 +95,14 @@ export class App extends Component {
         <ContactForm onSubmit={this.onFormSubmit} />
         <ContactsTitle>Contacts</ContactsTitle>
         <Filter filter={filter} onHandleSearch={this.onSearch} />
+
         <ContactList
           contacts={displayedContacts}
           onHandleDelete={this.contactDelete}
         />
+        <ButtonStyled onClick={this.restoreDeleted}>
+          Restore deleted
+        </ButtonStyled>
         <GlobalStyle />
       </ConteinerStyled>
     );
